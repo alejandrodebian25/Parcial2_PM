6e63554c0ea476416a66da77710bfc687516b993
Aleks <alejandrodebian25@gmail.com>
2020-07-31 13:55:53 -0400

    Agregando pregunta2 Contorno de Imagen con Matriz

---
+++ Pregunta2/ContornoImagen/ContornoImagen/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ContornoImagen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(631, 102);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(398, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CONTORNO DE UNA IMAGEN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(1, 108);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(631, 526);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(441, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Carga Imagen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(441, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Obtener Contorno";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(71, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(489, 503);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 635);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Contorno Imagen";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Pregunta2/ContornoImagen/ContornoImagen/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContornoImagen {
+    public partial class Form1 : Form {
+        Bitmap bmp;
+        int[, ] MatrizC = new int[3, 3];
+        int[, ] MatrizV = new int[3, 3];
+        public Form1 () {
+            InitializeComponent ();
+        }
+
+        private void label1_Click (object sender, EventArgs e) { }
+
+        private void button1_Click (object sender, EventArgs e) {
+            openFileDialog1.Filter = "Archivo JPG|*.jpg|Tdos los archivos|*.*";
+            openFileDialog1.ShowDialog ();
+            bmp = new Bitmap (openFileDialog1.FileName);
+            pictureBox1.Image = bmp;
+        }
+
+        private void button2_Click (object sender, EventArgs e) {
+            Bitmap bmp1 = new Bitmap (bmp.Width, bmp.Height);
+            Bitmap bmp2 = new Bitmap (bmp.Width, bmp.Height);
+
+            int nuevopixel = 0;
+
+            // Definiendo la matriz  Mascara de convulsion de 3x3 para contorno 
+            MatrizC[0, 0] = 0;
+            MatrizC[0, 1] = 1;
+            MatrizC[0, 2] = 0;
+
+            MatrizC[1, 0] = 1;
+            MatrizC[1, 1] = -4;
+            MatrizC[1, 2] = 1;
+
+            MatrizC[2, 0] = 0;
+            MatrizC[2, 1] = 1;
+            MatrizC[2, 2] = 0;
+
+            for (int i = 1; i < bmp.Width - 1; i++) {
+                for (int j = 1; j < bmp.Height - 1; j++) {
+                    // obteniendo la matriz Ventana de convulcion de 3x3
+                    MatrizV[0, 0] = GetPixel (i - 1, j - 1);
+                    MatrizV[0, 1] = GetPixel (i - 1, j + 0);
+                    MatrizV[0, 2] = GetPixel (i - 1, j + 1);
+
+                    MatrizV[1, 0] = GetPixel (i, j - 1);
+                    MatrizV[1, 1] = GetPixel (i, j + 0);
+                    MatrizV[1, 2] = GetPixel (i, j + 1);
+
+                    MatrizV[2, 0] = GetPixel (i + 1, j - 1);
+                    MatrizV[2, 1] = GetPixel (i + 1, j + 0);
+                    MatrizV[2, 2] = GetPixel (i + 1, j + 1);
+
+                    nuevopixel = productoMatrices ();
+
+                    bmp1.SetPixel (i, j, Color.FromArgb (nuevopixel, nuevopixel, nuevopixel));
+                }
+            }
+            // dibujando la imagen
+            pictureBox1.Image = bmp1;
+
+        }
+
+        private int GetPixel (int i, int j) {
+            // esta funcion nos ayudara solo a trabajar un una intensidad de RGB
+            int intensidad;
+            Color c1 = new Color ();
+            c1 = bmp.GetPixel (i, j);
+            intensidad = (c1.R + c1.G + c1.B) / 3;
+            return intensidad;
+        }
+        private int productoMatrices () {
+            // multiplicando la matriz 3x3
+            int nuevopixel = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++)
+                    nuevopixel = nuevopixel + (MatrizC[i, j] * MatrizV[i, j]);
+            }
+            // nuevo pixel entre 0-255
+            if (nuevopixel < 0)
+                nuevopixel = 0;
+            if (nuevopixel > 255)
+                nuevopixel = 255;
+            return nuevopixel;
+        }
+
+    }
+}
